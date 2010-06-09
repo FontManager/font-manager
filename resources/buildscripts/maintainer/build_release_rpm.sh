6f03f9601d800def3eb9eb033f2c4f135dfe8847
Jerrycasiano <Jerrycasiano@0997c51c-9dd2-11de-9cec-99053a980508>
2010-06-09 19:44:25 +0000

    - Tweak build script

--- resources/buildscripts/maintainer/build_release_rpm.sh
+++ resources/buildscripts/maintainer/build_release_rpm.sh
@@ -2,12 +2,18 @@
 
 su -c 'yum update -y && yum groupinstall "Development Tools" "Fedora Packager" -y && yum install fontconfig-devel freetype-devel glib2-devel python-devel -y'
 
+clean='no'
+if [ ! -e ~/rpmbuild ]
+then
+    rpmdev-setuptree
+    clean='yes'
+fi
 echo
 echo 'Fetching source'
 echo
-svn co http://font-manager.googlecode.com/svn/trunk/ font-manager
+[ -e font-manager ] || svn co http://font-manager.googlecode.com/svn/trunk/ font-manager
 cd font-manager
-find . -name '.svn' -print | xargs rm -rf
+svn update
 . ./release
 echo
 echo 'Preparing source'
@@ -25,11 +31,16 @@ rm -rf RESULTS
 mkdir RESULTS
 mock --resultdir=./RESULTS/ -r fedora-$(cat /etc/system-release | tr -cd '[[:digit:]]')-$(arch) ~/rpmbuild/SRPMS/$PACKAGE*.src.rpm
 echo 'Now running cleanup'
-rm -f ~/rpmbuild/SRPMS/$PACKAGE*
-rm -f ~/rpmbuild/SPECS/$PACKAGE*
-rm -f ~/rpmbuild/SOURCES/$PACKAGE*
+if [ $clean = 'yes' ]
+then
+    rm -rf ~/rpmbuild
+else
+    rm -f ~/rpmbuild/SRPMS/$PACKAGE*
+    rm -f ~/rpmbuild/SPECS/$PACKAGE*
+    rm -f ~/rpmbuild/SOURCES/$PACKAGE*
+fi
+make distclean
 rm -f $PACKAGE-$VERSION.tar.bz2
-rm -rf ~/rpmbuild
 echo
 echo 'Done!'
 
