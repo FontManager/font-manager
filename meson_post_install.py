9104df115bc71adb2729b41a03da7ccf936314e8
Jerry Casiano <JerryCasiano@gmail.com>
2019-04-16 21:34:12 -0400

    - Fix possible KeyError in meson_post_install script

--- meson_post_install.py
+++ meson_post_install.py
@@ -7,7 +7,7 @@ prefix = environ['MESON_INSTALL_PREFIX']
 data_dir = path.join(prefix, 'share')
 schema_dir = path.join(data_dir, 'glib-2.0', 'schemas')
 
-if not environ['DESTDIR']:
+if not environ.get('DESTDIR'):
     print('Compiling gsettings schemas...')
     call(['glib-compile-schemas', schema_dir])
     print('Updating desktop database...')
