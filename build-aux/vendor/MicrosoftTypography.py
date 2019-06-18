04fc34ae6681abdcd9221213af4e8a74d67094ef
Jerry Casiano <JerryCasiano@gmail.com>
2019-06-17 22:32:35 -0400

    - Fix vendor header generation

--- build-aux/vendor/MicrosoftTypography.py
+++ build-aux/vendor/MicrosoftTypography.py
@@ -40,6 +40,6 @@ def list_vendors () :
     vendor_list = pd.read_html(URL)
     for entry in vendor_list:
         for vendor in entry.itertuples():
-            if vendor[INDEX] > 1:
+            if vendor[INDEX] > 0:
                 yield (vendor[ID], vendor[NAME])
     return
