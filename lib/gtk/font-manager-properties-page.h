42320be3c3e7ed20bf2024863134d95707fb7a14
Jerry Casiano <JerryCasiano@gmail.com>
2022-02-18 19:59:39 -0500

    - Add PropertiesPage widget

---
+++ lib/gtk/font-manager-properties-page.h
@@ -0,0 +1,34 @@
+/* font-manager-properties-page.h
+ *
+ * Copyright (C) 2009-2022 Jerry Casiano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ *
+ * If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+*/
+
+#pragma once
+
+#include <glib/gi18n-lib.h>
+#include <gtk/gtk.h>
+#include <pango/pango-layout.h>
+#include <json-glib/json-glib.h>
+
+#include "font-manager-gtk-utils.h"
+
+#define FONT_MANAGER_TYPE_PROPERTIES_PAGE (font_manager_properties_page_get_type ())
+G_DECLARE_FINAL_TYPE(FontManagerPropertiesPage, font_manager_properties_page, FONT_MANAGER, PROPERTIES_PAGE, GtkWidget)
+
+GtkWidget * font_manager_properties_page_new ();
+void font_manager_properties_page_update (FontManagerPropertiesPage *self, JsonObject *properties);
---
+++ test/tests/properties-page-test.c
@@ -0,0 +1,43 @@
+#include "test-application.h"
+#include "font-manager-properties-page.h"
+
+const gchar *DESCRIPTION = """ \
+Candara's verticals show both convex and concave curvature with entasis and ectasis on opposite \
+sides of stems, high-branching arcades in the lowercase, large apertures in all open forms, and \
+unique ogee curves on diagonals. Its italic includes many calligraphic and serif font influences, \
+which are common in modern sans-serif typefaces. Calibri and Corbel, from the same family, have \
+similar designs and spacing.""";
+
+JsonObject *
+get_test_props ()
+{
+    JsonObject *props = json_object_new();
+    json_object_set_string_member(props, "psname", "Candara");
+    json_object_set_string_member(props, "family", "Candara");
+    json_object_set_string_member(props, "style", "Regular");
+    json_object_set_string_member(props, "width", "Normal");
+    json_object_set_string_member(props, "slant", "Normal");
+    json_object_set_string_member(props, "weight", "Regular");
+    json_object_set_string_member(props, "spacing", "Proportional");
+    json_object_set_string_member(props, "version", "5.61");
+    json_object_set_string_member(props, "vendor", "Microsoft");
+    json_object_set_string_member(props, "filetype", "TrueType");
+    json_object_set_string_member(props, "filesize", "218.5kB");
+    json_object_set_string_member(props, "designer", "Gary Munch");
+    json_object_set_string_member(props, "designer-url", "http://www.munchfonts.com");
+    json_object_set_string_member(props, "description", DESCRIPTION);
+    json_object_set_string_member(props, "copyright", "2008 Microsoft Corporation. All Rights Reserved.");
+    return props;
+}
+
+G_MODULE_EXPORT
+TestDialog *
+get_widget (TestApplicationWindow *parent)
+{
+    TestDialog *dialog = test_dialog_new(parent, "Properties Page", 600, 450);
+    GtkWidget *props = font_manager_properties_page_new();
+    g_autoptr(JsonObject) _props = get_test_props();
+    font_manager_properties_page_update(FONT_MANAGER_PROPERTIES_PAGE(props), _props);
+    test_dialog_append(dialog, props);
+    return dialog;
+}
